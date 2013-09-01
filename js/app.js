4ca6a4c1f129008264f67b818079c558a975ddaf
Stuart Quin <stuart.quin@gmail.com>
2013-09-01 20:00:56 +0100

    WIP Create New form

--- js/app.js
+++ js/app.js
@@ -14,13 +14,16 @@ App.Store = DS.Store.extend({
 
 App.Router.map(function() {
   this.resource("users", function(){
-    this.resource("user", {path: ":_id"});
-    this.route("add");
+    this.resource("user", {path: ":_id"}, function(){
+      this.route("edit");
+    });
+    this.route("new");
   });
 });
 
 App.User = DS.Model.extend({
   email: DS.attr("string"),
+  name: DS.attr("string"),
   password: DS.attr("string"),
   created_at: DS.attr("date"),
   login_attempts: DS.attr("number")
@@ -38,18 +41,38 @@ App.UserRoute = Ember.Route.extend({
   }
 });
 
+/**
+ * 'New User' Route, says that we want the same template as edit route
+ */
+App.UsersNewRoute = Ember.Route.extend({
+  setupController: function(controller, model){
+    this.controllerFor("user").set("model", App.User.createRecord());
+  },
+  renderTemplate: function(){
+    this.render("user._edit");
+  }
+});
+
+/**
+ * UserController handles both editing and viewing of existing users
+ */
 App.UserController = Ember.ObjectController.extend({
   isEditing: false,
+  isNew: false,
   actions: {
     toggleEdit: function(){
       this.set("isEditing", !this.isEditing);
     },
-    save: function(){
+    save: function(user){
       this.set("isEditing", false);
+      user.transaction.commit();
     }
   }
 });
 
 Ember.Handlebars.helper("format-date", function(date){
+  if (!date){
+    return "";
+  }
   return moment(date).format('YYYY-MM-DD, HH:mm:ss');
 });
